abe467edbbb8884aec295df7fd95ad0b6bdf3e71
danieldemedziuk <danieldemedziuk@gmail.com>
2022-05-01 12:35:03 +0200

    added movemoent of my red box

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Player {
 
   update() {
     this.draw()
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
     
     if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -37,12 +38,58 @@ class Player {
 }
 
 const player = new Player()
-
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+
+    // move keys 'a' and 'd'
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
 }
 
 animate()
+
+window.addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65: 
+          keys.left.pressed = true
+          break
+        case 68: 
+          keys.right.pressed = true
+          break
+        case 87: 
+          player.velocity.y -= 20
+          break
+        case 83: 
+          break
+    }
+})
+
+window.addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65: 
+          keys.left.pressed = false
+          break
+        case 68: 
+          keys.right.pressed = false
+          break
+        case 87: 
+        //   player.velocity.y -= 5
+          break
+        case 83: 
+          break
+    }
+})
